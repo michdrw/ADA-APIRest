b3c5d91b3ae3962aa89975a6f411a2137688cf7d
michdrw <michdrw@gmail.com>
2019-10-15 13:41:55 -0300

    Commit para que lo continue Dayren

--- moverest/src/main/java/ar/com/ada/api/moverest/controllers/AuthController.java
+++ moverest/src/main/java/ar/com/ada/api/moverest/controllers/AuthController.java
@@ -1,9 +1,34 @@
 package ar.com.ada.api.moverest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.moverest.services.UsuarioService;
+
 /**
  * AuthController
  */
+@RestController
 public class AuthController {
 
-    
+    @Autowired
+    UsuarioService usuarioService;
+
+    @PostMapping("auth/register")
+    public RegistrationResponse postRegisterUser(@RequestBody RegistrationRequest req) {
+
+
+        RegistrationResponse r = new RegistrationResponse();
+        int usuarioCreadoId = usuarioService.crearUsuario(req.nombre, req.dni, req.edad, req.email, req.password);
+        
+
+        r.isOk = true;
+        r.message = "Te registraste con exito";
+        r.usuarioId = usuarioCreadoId; 
+
+        return r;
+        
+    }
+
 }
