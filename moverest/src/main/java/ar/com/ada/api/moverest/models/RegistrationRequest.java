4e3daee22a8b9e40a14f90f4812745dfc20633e7
dayrenl <dayrenlivcar@gmail.com>
2019-10-15 14:00:17 -0300

    Models y complemento de auth.

---
+++ moverest/src/main/java/ar/com/ada/api/moverest/models/RegistrationRequest.java
@@ -0,0 +1,15 @@
+package ar.com.ada.api.moverest.models;
+
+/**
+ * RegistrationRequest
+ */
+public class RegistrationRequest {
+
+    public String nombre;
+    public int edad;
+    public String tipoIdentificacion;
+    public int nroIdentificacion;
+    public String email;
+    public String password;
+    
+}
---
+++ moverest/src/main/java/ar/com/ada/api/moverest/models/RegistrationResponse.java
@@ -0,0 +1,12 @@
+package ar.com.ada.api.moverest.models;
+
+/**
+ * RegistrationResponse
+ */
+public class RegistrationResponse {
+
+    public boolean isOk;
+    public String message;
+    public Integer usuarioId;
+
+}
