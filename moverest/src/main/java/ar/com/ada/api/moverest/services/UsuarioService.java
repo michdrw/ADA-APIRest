fab47412c6d3680229a7b5d753ad1cb630116405
dayrenl <dayrenlivcar@gmail.com>
2019-10-21 23:44:07 -0300

    Clase crypto agregada.

--- moverest/src/main/java/ar/com/ada/api/moverest/services/UsuarioService.java
+++ moverest/src/main/java/ar/com/ada/api/moverest/services/UsuarioService.java
@@ -9,6 +9,7 @@ import ar.com.ada.api.moverest.entities.Inmobiliaria;
 import ar.com.ada.api.moverest.entities.Locatario;
 import ar.com.ada.api.moverest.entities.Usuario;
 import ar.com.ada.api.moverest.repositories.UsuarioRepository;
+import ar.com.ada.api.moverest.security.Crypto;
 
 /**
  * UsuarioService
@@ -23,51 +24,58 @@ public class UsuarioService {
     @Autowired
     InmobiliariaService inmobiliariaService;
 
-    public void save(Usuario u) 
-    {
+    public void save(Usuario u) {
         repoUsuario.save(u);
     }
 
-    public int crearUsuario(String email, String password, Boolean tipo, String nombre, int edad, String tipoIdentificacion, int nroIdentificacion){
-    
+    public int crearUsuario(String email, String password, Boolean tipo, String nombre, int edad,
+            String tipoIdentificacion, int nroIdentificacion) {
+
+        String passwordEncriptada;
+        String passwordEnTextoClaroDesencriptado;
+        String passwordEnTextoClaro;
         Usuario u = new Usuario();
         u.setEmail(email);
         u.setUsername(email);
-        u.setPassword(password);
-        save(u);
 
-        if (tipo == true)
-        {
-        Locatario l = new Locatario();
-        l.setNombre(nombre);
-        l.setTipoIdentificacion(tipoIdentificacion);
-        l.setNroIdentificacion(nroIdentificacion);
-        l.setEdad(edad);
-        l.setUsuario(u);
-        
+        passwordEnTextoClaro = password;
+        passwordEncriptada = Crypto.encrypt(passwordEnTextoClaro, u.getUsername());
+        passwordEnTextoClaroDesencriptado = Crypto.decrypt(passwordEncriptada, u.getUsername());
+
+        if (passwordEnTextoClaro.equals(passwordEnTextoClaroDesencriptado)) {
+            
+        u.setPassword(passwordEncriptada);
         save(u);
-        locatarioService.save(l);
-        }
-        else
-        {
-            Inmobiliaria mr = new Inmobiliaria(); 
-            mr.setUsuario(u);
-            inmobiliariaService.save(mr);
-            u.setInmobiliaria(mr);
-            save(u);
+            if (tipo == true) {
+                Locatario l = new Locatario();
+                l.setNombre(nombre);
+                l.setTipoIdentificacion(tipoIdentificacion);
+                l.setNroIdentificacion(nroIdentificacion);
+                l.setEdad(edad);
+                l.setUsuario(u);
+
+                save(u);
+                locatarioService.save(l);
+            } else {
+                Inmobiliaria mr = new Inmobiliaria();
+                mr.setUsuario(u);
+                inmobiliariaService.save(mr);
+                u.setInmobiliaria(mr);
+                save(u);
+            }
         }
 
         return u.getUsuarioId();
     }
 
-      public Usuario buscarPorId(Integer id) {
+    public Usuario buscarPorId(Integer id) {
         Optional<Usuario> u = repoUsuario.findById(id);
         if (u.isPresent())
             return u.get();
         return null;
     }
 
-     public int modificaPassword(Integer usuarioId, String password) {
+    public int modificaPassword(Integer usuarioId, String password) {
         Usuario u = this.buscarPorId(usuarioId);
         u.setUsuarioId(usuarioId);
         u.setPassword(password);
